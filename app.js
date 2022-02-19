e853d659d5f389315f14474f2d905282c8733b6a
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2022-02-19 09:46:19 +0300

    app

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-function tableCreate() {
-    //body reference 
-    var body = document.getElementsByTagName("body")[0];
-  
-    // create elements <table> and a <tbody>
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    // cells creation
-    for (var j = 0; j <= 2; j++) {
-      // table row creation
-      var row = document.createElement("tr");
-  
-      for (var i = 0; i < 2; i++) {
-        // create element <td> and text node 
-        //Make text node the contents of <td> element
-        // put <td> at end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode("cell is row " + j + ", column " + i);
-  
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-  
-      //row added to end of table body
-      tblBody.appendChild(row);
-    }
-  
-    // append the <tbody> inside the <table>
-    tbl.appendChild(tblBody);
-    // put <table> in the <body>
-    body.appendChild(tbl);
-    // tbl border attribute to 
-    tbl.setAttribute("border", "2");
-  }
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Adding the entire table to the body tag
+document.getElementById('body').appendChild(table);
+
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Sr. No.";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Name";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "Company";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+thead.appendChild(row_1);
+
+// Creating and adding data to second row of the table
+let row_2 = document.createElement('tr');
+let row_2_data_1 = document.createElement('td');
+row_2_data_1.innerHTML = "1.";
+let row_2_data_2 = document.createElement('td');
+row_2_data_2.innerHTML = "James Clerk";
+let row_2_data_3 = document.createElement('td');
+row_2_data_3.innerHTML = "Netflix";
+
+row_2.appendChild(row_2_data_1);
+row_2.appendChild(row_2_data_2);
+row_2.appendChild(row_2_data_3);
+tbody.appendChild(row_2);
+
+
+// Creating and adding data to third row of the table
+let row_3 = document.createElement('tr');
+let row_3_data_1 = document.createElement('td');
+row_3_data_1.innerHTML = "2.";
+let row_3_data_2 = document.createElement('td');
+row_3_data_2.innerHTML = "Adam White";
+let row_3_data_3 = document.createElement('td');
+row_3_data_3.innerHTML = "Microsoft";
+
+row_3.appendChild(row_3_data_1);
+row_3.appendChild(row_3_data_2);
+row_3.appendChild(row_3_data_3);
+tbody.appendChild(row_3);
